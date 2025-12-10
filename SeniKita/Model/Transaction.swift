3f89816f10b0ebfe42cff7938b8305dee2084b95
Ricky Primayuda Putra <rickyprima30@gmail.com>
2025-12-10 12:20:37 +0700

    feat: add custom decoder to handle order_id type mismatch in Transaction model

--- SeniKita/Model/Transaction.swift
+++ SeniKita/Model/Transaction.swift
@@ -14,3 +14,25 @@ struct Transaction: Codable {
     let created_at: String
     let updated_at: String
 }
+
+extension Transaction {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decode(Int.self, forKey: .id)
+        
+        // Handle order_id as String or Int
+        if let strValue = try? container.decode(String.self, forKey: .order_id) {
+            order_id = Int(strValue) ?? 0
+        } else if let intValue = try? container.decode(Int.self, forKey: .order_id) {
+            order_id = intValue
+        } else {
+            order_id = 0
+        }
+        
+        payment_status = try container.decode(String.self, forKey: .payment_status)
+        payment_date = try? container.decode(String.self, forKey: .payment_date)
+        created_at = try container.decode(String.self, forKey: .created_at)
+        updated_at = try container.decode(String.self, forKey: .updated_at)
+    }
+}
