9da5060e8308907507f4f9c4f705e257e5830d7a
Ricky Primayuda Putra <rickyprima30@gmail.com>
2025-12-10 12:16:31 +0700

    refactor: use EnvironmentObject for AuthViewModel in GoogleButton

--- SeniKita/View/Auth/Components/GoogleButton.swift
+++ SeniKita/View/Auth/Components/GoogleButton.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct GoogleButton: View {
-    @ObservedObject var authViewModel = AuthViewModel()
+    @EnvironmentObject var authViewModel: AuthViewModel
     
     var body: some View {
         Button {
