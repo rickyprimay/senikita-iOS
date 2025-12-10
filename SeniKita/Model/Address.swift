466aae9ece2328f19c86fa82adee31477590c9dd
Ricky Primayuda Putra <rickyprima30@gmail.com>
2025-12-10 12:12:45 +0700

    fix: handle inconsistent data types in Address model

--- SeniKita/Model/Address.swift
+++ SeniKita/Model/Address.swift
@@ -24,10 +24,57 @@ struct Address: Codable {
     let address_detail: String
     let province_id: Int
     let city_id: Int
-    let postal_code: Int
+    let postal_code: String
     let note: String?
     let created_at: String
     let updated_at: String
     let city: City?
     let province: Province?
+}
+
+extension Address {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decode(Int.self, forKey: .id)
+        
+        // Handle user_id as String or Int
+        if let strValue = try? container.decode(String.self, forKey: .user_id) {
+            user_id = Int(strValue) ?? 0
+        } else if let intValue = try? container.decode(Int.self, forKey: .user_id) {
+            user_id = intValue
+        } else {
+            user_id = 0
+        }
+        
+        label_address = try container.decode(String.self, forKey: .label_address)
+        name = try container.decode(String.self, forKey: .name)
+        phone = try container.decode(String.self, forKey: .phone)
+        address_detail = try container.decode(String.self, forKey: .address_detail)
+        
+        // Handle province_id as String or Int
+        if let strValue = try? container.decode(String.self, forKey: .province_id) {
+            province_id = Int(strValue) ?? 0
+        } else if let intValue = try? container.decode(Int.self, forKey: .province_id) {
+            province_id = intValue
+        } else {
+            province_id = 0
+        }
+        
+        // Handle city_id as String or Int
+        if let strValue = try? container.decode(String.self, forKey: .city_id) {
+            city_id = Int(strValue) ?? 0
+        } else if let intValue = try? container.decode(Int.self, forKey: .city_id) {
+            city_id = intValue
+        } else {
+            city_id = 0
+        }
+        
+        postal_code = try container.decode(String.self, forKey: .postal_code)
+        note = try? container.decode(String.self, forKey: .note)
+        created_at = try container.decode(String.self, forKey: .created_at)
+        updated_at = try container.decode(String.self, forKey: .updated_at)
+        city = try? container.decode(City.self, forKey: .city)
+        province = try? container.decode(Province.self, forKey: .province)
+    }
 }
