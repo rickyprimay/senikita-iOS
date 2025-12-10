ae1222f7d452a719474b05a64f51da5264f6b764
Ricky Primayuda Putra <rickyprima30@gmail.com>
2025-12-10 12:16:16 +0700

    feat: handle province_id as String or Int in City model

--- SeniKita/Model/City.swift
+++ SeniKita/Model/City.swift
@@ -21,3 +21,27 @@ struct City: Codable, Identifiable, Hashable {
     let created_at: String?
     let updated_at: String?
 }
+
+extension City {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decode(Int.self, forKey: .id)
+        
+        // Handle province_id as String or Int
+        if let strValue = try? container.decode(String.self, forKey: .province_id) {
+            province_id = Int(strValue)
+        } else if let intValue = try? container.decode(Int.self, forKey: .province_id) {
+            province_id = intValue
+        } else {
+            province_id = nil
+        }
+        
+        type = try? container.decode(String.self, forKey: .type)
+        name = try container.decode(String.self, forKey: .name)
+        postal_code = try? container.decode(String.self, forKey: .postal_code)
+        province = try? container.decode(Province.self, forKey: .province)
+        created_at = try? container.decode(String.self, forKey: .created_at)
+        updated_at = try? container.decode(String.self, forKey: .updated_at)
+    }
+}
